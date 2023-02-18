870b64e10735f57cdf9a89a44a65df44cadbdedc
Jacob Zarins <jzarins2@student.cvtc.edu>
2023-02-17 18:48:27 -0600

    Changed server.js to standard style

--- server.js
+++ server.js
@@ -1,47 +1,47 @@
 const newFolderName = function (folders) {
-    let genNewFolder = true
+  let genNewFolder = true
 
-    if (folders.length == 0) {
-        return 'New Folder'
-    }
+  if (folders.length === 0) {
+    return 'New Folder'
+  }
 
-    for (i = 0; i < folders.length; i++) {
-        if (folders[i] == 'New Folder') {
-            genNewFolder = false
-            i = folders.length
-        }
+  for (let i = 0; i < folders.length; i++) {
+    if (folders[i] === 'New Folder') {
+      genNewFolder = false
+      i = folders.length
     }
+  }
 
-    if (genNewFolder == true) {
-        return 'New Folder'
-    }  else {
-        let n = 2
-        for (i = 0; i < folders.length; i++) {
-            if (folders[i] == "New Folder (" + n + ")") {
-                n++
-                i = -1
-            }
-        }
-        return ("New Folder (" + n + ")")
+  if (genNewFolder === true) {
+    return 'New Folder'
+  } else {
+    let n = 2
+    for (let i = 0; i < folders.length; i++) {
+      if (folders[i] === 'New Folder (' + n + ')') {
+        n++
+        i = -1
+      }
     }
+    return ('New Folder (' + n + ')')
+  }
 }
 
-
 const folders1 = [
-    'New Folder (4)',
-    'New Folder (2)',
-    'New Folder',
-    'New Folder (3)',
-    'New Folder (6)'
-  ]
+  'New Folder (4)',
+  'New Folder (2)',
+  'New Folder',
+  'New Folder (3)',
+  'New Folder (6)'
+]
 
-  const folders2 = [
-    'New',
-    'Folder that is new',
-    'New Folder (7)',
-    'New Folder'
-  ]
+const folders2 = [
+  'New',
+  'Folder that is new',
+  'New Folder (7)',
+  'New Folder'
+]
 
 console.log(newFolderName(folders1))
+console.log(newFolderName(folders2))
 
 module.exports = { newFolderName }
